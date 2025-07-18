fa08fc77294578f5db7b34df6c7ce75a087a946e
v0idgy <student-01-cea12fdbe306@qwiklabs.net>
2025-07-18 10:59:37 +0000

    Deploy v2.0 to production environment with /red endpoint

--- main.go
+++ main.go
@@ -16,11 +16,6 @@ func blueHandler(w http.ResponseWriter, r *http.Request) {
 	png.Encode(w, img)
 }
 
-func main() {
-	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler) # Add this line
-	http.ListenAndServe(":8080", nil)
-}
 
 func redHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
@@ -28,6 +23,15 @@ func redHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
+
+
+
+func main() {
+	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler) # Add this line
+	http.ListenAndServe(":8080", nil)
+}
+
 
 
 
